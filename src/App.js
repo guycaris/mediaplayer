1dce36f7089f0501538412224351b044bcb3cafd
guycaris <guycaris@yahoo.com>
2024-03-30 23:24:29 +0000

    adding dockerfile

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Player2 from './components/Player2';
 const videoClips = ['./clips/clip1.mp4', './clips/clip2.mp4', './clips/clip3.mp4', './clips/clip4.mp4', './clips/clip5.mp4'];
 
 function App() {
-
-
   
   return (
     <div className="App">
